9011e377b30b3f747076fa1ac2b41baaa8dd7159
RaccoNet <zavolokinyegor@gmail.com>
2026-03-08 07:40:44 +0100

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="table-print",
+    version="1.0.0",
+    description="Print data as formatted ASCII tables",
+    long_description=open("README.md", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+    author="raccoonette",
+    url="https://github.com/MopicMP/table-print",
+    packages=find_packages(),
+    python_requires=">=3.8",
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Topic :: Software Development :: Libraries",
+    ],
+)
